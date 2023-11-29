bf8a19443af19d730b54a61a3c4ac3ea521c171f
shro-2002 <96790181+shro-2002@users.noreply.github.com>
2023-11-30 01:05:40 +0530

    Added UC-2

--- src/main/java/com/bridgelabs/HashTables/Main.java
+++ src/main/java/com/bridgelabs/HashTables/Main.java
@@ -3,7 +3,7 @@ package com.bridgelabs.HashTables;
 public class Main {
 
 	public static void main(String[] args) {
-		String s = "To be or not to be";
+		String s = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
 
 		HashTable<String, Integer> table = new HashTable<>(20);
 
